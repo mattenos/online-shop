5735684c5caecb341bd862f379a1572035e13489
Matt Enos <89925779+mattenos@users.noreply.github.com>
2022-02-23 13:07:38 -0500

    Cleaned up the server.js to remove extra code that wasn't being used.

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # online-shop
 
-.set up server
+.set up server.js
 .download packages
-.set up config/connections/dotenv
-.create database
-.create api routes
+-dotenv
+-express
+-sequelize
+-mysql2
+-bcrypt
+
+.set up config folder
+
+.create .env file and populate it with mysql
+
+.create db folder and schema
+
+.create models folder
+-user model for creating an account
+ - set up User table and columns
+
+.create controllers folder
+ -create index for controllers
+ -create api folder
+  -create index for api folder
+   -create userRoutes.js
--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 const express = require('express');
 const sequelize = require('./config/connection');
-const routes = require('./controllers');
-const path = require('path');
 
 const app = express();
-
-const User = require('./models/User');
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/api', routes);
-
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'index.html'));
-});
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 sequelize.sync({ force: true }).then(() => {
 	app.listen(PORT, () => {
