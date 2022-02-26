d1637d1b973820905f2443e6ab3e69343b3481ac
Matt Enos <89925779+mattenos@users.noreply.github.com>
2022-02-26 14:58:53 -0500

    Added server functionality.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const sequelize = require('./config/connection');
+const routes = require('./controllers');
+
 
 const app = express();
 
@@ -8,6 +10,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/'));
+
+app.use(routes);
 
 sequelize.sync({ force: true }).then(() => {
 	app.listen(PORT, () => {
